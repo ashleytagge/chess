dd6eee2770d17df1f2a77ca2f4ec3311d579cd80
Ashley Tagge <ashley70@byu.edu>
2026-01-13 22:54:57 -0700

    Knight functionality and Create MovesGenerator Class to organize and get rid of redundant code. clean up!

---
+++ shared/src/main/java/chess/MovesGenerator.java
@@ -0,0 +1,42 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MovesGenerator {
+    //move redundant code into this class so it only appears once
+    //Rook
+    public Collection<ChessMove> MoveMultipleLeft(){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        return validMoves;
+    }
+    public Collection<ChessMove> MoveMultipleRight(){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        return validMoves;
+    }
+    public Collection<ChessMove> MoveMultipleUp(){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        return validMoves;
+    }
+    public Collection<ChessMove> MoveMultipleDown(){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        return validMoves;
+    }
+    //Bishop
+    public Collection<ChessMove> MoveMultipleUpLeft(){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        return validMoves;
+    }
+    public Collection<ChessMove> MoveMultipleUpRight(){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        return validMoves;
+    }
+    public Collection<ChessMove> MoveMultipleDownLeft(){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        return validMoves;
+    }
+    public Collection<ChessMove> MoveMultipleDownRight(){
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        return validMoves;
+    }
+}
