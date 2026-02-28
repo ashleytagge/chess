705eb89409dd0b8e9abd346549a867f9e7da2505
Ashley Tagge <ashley70@byu.edu>
2026-02-28 01:25:14 -0700

    MemoryGameDAO getGame and clearGames

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -5,27 +5,10 @@ import model.GameData;
 import java.util.List;
 
 public interface GameDAO {
-    /*
-    Add Game
-    Add Player to Game
-    GetAllGames
-    GetGameName
-
-    Create objects in the data store
-    Read objects from the data store
-    Update objects already in the data store
-    Delete objects from the data store
-     */
     void createGame() throws DataAccessException;
     GameData getGame(int gameID) throws DataAccessException;
     List<GameData> getAllGames() throws DataAccessException;
     void addPlayer() throws DataAccessException;
     void updateGame(int gameID) throws DataAccessException;
-        /*
-        * It should replace the chess game
-        * string corresponding to a given
-        * gameID. This is used when players
-        * join a game or when a move is made.
-        */
     void clearGames() throws DataAccessException;
 }
--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class MemoryGameDAO {
+
+    final private HashMap<Integer, GameData> games = new HashMap<>();
+
+    void createGame(int gameID) {
+
+    }
+
+    GameData getGame(int gameID) throws DataAccessException {
+        if(games.containsKey(gameID)){
+            return games.get(gameID);
+        }else{
+            throw new DataAccessException("game ID does not exist");
+        }
+    }
+    List<GameData> getAllGames() {}
+    void addPlayer() {}
+    void updateGame(int gameID) {}
+    void clearGames() {
+        games.clear();
+    }
 }
