8367e9725a261d6d3af27d8e26e4b966b207096f
Ashley Tagge <ashley70@byu.edu>
2026-02-28 19:27:52 -0700

    debug gameID generator

--- server/src/main/java/service/GenerateGameID.java
+++ server/src/main/java/service/GenerateGameID.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 public class GenerateGameID {
 
+    public static int gameID = 1000;
+
     public static int generateGameID(){
-        int gameID = 1000;
         return gameID++;
     }
 }
