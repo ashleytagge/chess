aa12970fcef2e771553c5d9f36efd87827f5141f
Ashley Tagge <ashley70@byu.edu>
2026-04-06 23:42:50 -0600

    change petshop map to hashmap with set of sessions with gameID as key

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 //implement sessions differently, use gameID
@@ -11,10 +12,16 @@ import java.util.concurrent.ConcurrentHashMap;
 //store a map and use the gameID as the key and the value is a set of sessions that belong to that game
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
 
-    public void add(Session session) {
-        connections.put(session, session);
+    public final ConcurrentHashMap<Integer, Set<Session>> connections = new ConcurrentHashMap<>();
+
+    public void add(int gameID, Session session) {
+        Set<Session> currentSessions = connections.get(gameID);
+        if(currentSessions == null){
+            currentSessions = ConcurrentHashMap.newKeySet();
+            Set<Session> currentConnections = connections.putIfAbsent(gameID, currentSessions);
+        }
+        currentSessions.add(session);
     }
 
     public void remove(Session session) {
